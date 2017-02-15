400744348c2177237006300efd972711cc34b37a
Yoko  Ishizaki <yi35143@srcs.loc>
2017-02-15 14:07:03 -0800

    UC12

---
+++ functions.js
@@ -0,0 +1,70 @@
+/**
+* Removes part of string between two sub strings
+* @param {string} text The original string
+* @param {string} start The starting string
+* @param {string} end The ending string
+* @return {string} The string in between
+* @throws (Error} If start or end not found
+*/
+function between(string, start, end) {
+    var startAt = string.indexOf(start);
+
+    if (startAt == -1) {
+        throw new Error("No start found: " + start);
+    }
+
+    startAt += start.length;
+    var endAt = string.indexOf(end, startAt);
+
+    if (endAt == -1) {
+        throw new Error("No end found: " + end);
+    }
+
+    return string.slice(startAt, endAt);
+}
+
+/**
+ * Returns an area code from a phone number: (###) ###-####
+ * @param   {string} phoneNum The phone number
+ * @returns {string} The area code
+ * @throws {Error} If the format is incorrect
+ */
+function getAreaCode(phoneNum) {
+
+    var areaCode;
+
+    try {
+        areaCode = between(phoneNum, "(", ")");
+        areaCode = areaCode.trim();
+        if (areaCode.length == 3 && Number(areaCode)) {
+            return areaCode;
+        } else {
+            throw new Error("Invalid area code: " + areaCode);
+        }
+    } catch (error) {
+        throw new Error("Invalid phone number: " + error.message);
+    }
+}
+
+/**
+ * Displays the area code for an inputted phone number
+ * @param {string} inputId  The element id for the text box
+ * @param {string} outputId The element id of message div
+ */
+function displayAreaCode(inputId, outputId) {
+    var outputText = "";
+    var phoneNum = document.getElementById(inputId).value;
+
+    // Now try to get the code
+    try {
+        var areaCode = getAreaCode(phoneNum);
+        outputText = "Your area code is " + areaCode;
+    } catch (error) {
+        console.log(error.message);
+        outputText = error.message;
+    }
+
+    document.getElementById(outputId).innerHTML = outputText;
+}
+
+
---
+++ tests.js
@@ -0,0 +1,5 @@
+QUnit.test("Test the getAreaCode function.", function (assert) {
+    var num = "(415) 555-5555";
+    var result = getAreaCode(num);
+    assert.deepEqual(result, "415", "Valid area code test passed.");
+});
