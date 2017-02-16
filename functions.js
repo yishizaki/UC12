0a36f9d901fd15574aaa53125c27ffc401a3f65c
Chloe Thies <ChloeThies@Chloes-MacBook.local>
2017-02-16 14:53:53 -0800

    beautified

--- functions.js
+++ functions.js
@@ -1,28 +1,23 @@
 /**
-* Removes part of string between two sub strings
-* @param {string} text The original string
-* @param {string} start The starting string
-* @param {string} end The ending string
-* @return {string} The string in between
-* @throws (Error} If start or end not found
-*/
+ * Removes part of string between two sub strings
+ * @param {string} text The original string
+ * @param {string} start The starting string
+ * @param {string} end The ending string
+ * @return {string} The string in between
+ * @throws (Error} If start or end not found
+ */
 function between(string, start, end) {
     var startAt = string.indexOf(start);
-
     if (startAt == -1) {
         throw new Error("No start found: " + start);
     }
-
     startAt += start.length;
     var endAt = string.indexOf(end, startAt);
-
     if (endAt == -1) {
         throw new Error("No end found: " + end);
     }
-
     return string.slice(startAt, endAt);
 }
-
 /**
  * Returns an area code from a phone number: (###) ###-####
  * @param   {string} phoneNum The phone number
@@ -30,22 +25,21 @@ function between(string, start, end) {
  * @throws {Error} If the format is incorrect
  */
 function getAreaCode(phoneNum) {
-
     var areaCode;
-
     try {
         areaCode = between(phoneNum, "(", ")");
         areaCode = areaCode.trim();
         if (areaCode.length == 3 && Number(areaCode)) {
             return areaCode;
-        } else {
+        }
+        else {
             throw new Error("Invalid area code: " + areaCode);
         }
-    } catch (error) {
+    }
+    catch (error) {
         throw new Error("Invalid phone number: " + error.message);
     }
 }
-
 /**
  * Displays the area code for an inputted phone number
  * @param {string} inputId  The element id for the text box
@@ -54,17 +48,14 @@ function getAreaCode(phoneNum) {
 function displayAreaCode(inputId, outputId) {
     var outputText = "";
     var phoneNum = document.getElementById(inputId).value;
-
     // Now try to get the code
     try {
         var areaCode = getAreaCode(phoneNum);
         outputText = "Your area code is " + areaCode;
-    } catch (error) {
+    }
+    catch (error) {
         console.log(error.message);
         outputText = error.message;
     }
-
     document.getElementById(outputId).innerHTML = outputText;
 }
-
-
